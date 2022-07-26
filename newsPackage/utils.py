2ede9413c978b7af5b903b0d17991f2f420efd9a
Minji Son <limetree06@unist.ac.kr>
2022-07-26 10:43:16 +0900

    ADD vscode setting

---
+++ newsPackage/utils.py
@@ -0,0 +1,12 @@
+TYPE_CONFIG = {
+    "사회": "society",
+    "정치": "politics",
+    "경제": "economic",
+    "국제": "foreign",
+    "문화": "culture",
+    "연예": "entertain",
+    "스포츠": "sports",
+    "IT": "digital",
+    "칼럼": "editorial",
+    "보도자료": "press",
+}
